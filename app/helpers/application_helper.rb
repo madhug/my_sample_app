68d58b6b3db69a08d6b481037bf4c559cbb47783
madhuvanthi <madhupramod@gmail.com>
2013-02-08 12:38:18 -0500

    add dynamic headers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	def full_title(page_title)
+		base_title = "Sample Applicatin Ho!"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
+
 end
