cce4e23c8828493a0f2b420be65f8020e7ea2460
madhug <madhupramod@gmail.com>
2013-02-12 18:17:45 -0500

    throbbing test

--- app/assets/javascripts/circles.js
+++ app/assets/javascripts/circles.js
@@ -8,8 +8,18 @@
         circle.fillColor = 'red';
         circle.strokeColor = "black";
         var date = new Date();
+        var grow = true;
         view.onFrame = function(event) {
 
-            circle.scale(1 + (Math.sin(date.getMilliseconds())) * 0.1);
+            if(grow)
+            {
+                circle.scale(1 + (Math.sin(date.getMilliseconds())) * 0.1);
+                grow = false;
+            }
+            else
+            {
+                circle.scale(1 - (Math.sin(date.getMilliseconds())) * 0.1)
+                grow = true;
+            }
         }
     };
