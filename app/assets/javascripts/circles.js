790b074b8080aff27e1e9d4d08383d5affb2b1f1
madhug <madhupramod@gmail.com>
2013-02-12 16:26:19 -0500

    add paperjs features

--- app/assets/javascripts/circles.js
+++ app/assets/javascripts/circles.js
@@ -1,19 +1,36 @@
+  paper.install(window);
+    // Keep global references to both tools, so the HTML
+    // links below can access them.
+    var tool1, tool2;
 
-paper.install(window);
-window.onload = function() {
-    paper.setup('canvas');
-    // Create a simple drawing tool:
-    var tool = new Tool();
-    var path;
+    window.onload = function() {
+        paper.setup('myCanvas');
 
-    // Define a mousedown and mousedrag handler
-    tool.onMouseDown = function(event) {
-        path = new Path();
-        path.strokeColor = 'black';
-        path.add(event.point);
-    }
+        // Create two drawing tools.
+        // tool1 will draw straight lines,
+        // tool2 will draw clouds.
 
-    tool.onMouseDrag = function(event) {
-        path.add(event.point);
-    }
-}
+        // Both share the mouseDown event:
+        var path;
+        function onMouseDown(event) {
+            path = new Path();
+            path.strokeColor = 'black';
+            path.add(event.point);
+        }
+
+        tool1 = new Tool();
+        tool1.onMouseDown = onMouseDown;
+
+        tool1.onMouseDrag = function(event) {
+            path.add(event.point);
+        }
+
+        tool2 = new Tool();
+        tool2.minDistance = 20;
+        tool2.onMouseDown = onMouseDown;
+
+        tool2.onMouseDrag = function(event) {
+            // Use the arcTo command to draw cloudy lines
+            path.arcTo(event.point);
+        }
+    };
