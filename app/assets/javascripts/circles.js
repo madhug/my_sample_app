080f8093fc7a1d4f536522599512f1850eb83277
madhug <madhupramod@gmail.com>
2013-02-13 17:26:36 -0500

    add springy.js and have fun with force graphs

--- app/assets/javascripts/circles.js
+++ app/assets/javascripts/circles.js
@@ -1,36 +1,12 @@
-  paper.install(window);
-    // Keep global references to both tools, so the HTML
-    // links below can access them.
-    var tool1, tool2;
+var graph = new Graph();
+// make some nodes
+var spruce = graph.newNode({label: 'Norway Spruce'});
+var fir = graph.newNode({label: 'Sicilian Fir'});
+var tada = graph.newNode({label: 'Tada'});
 
-    window.onload = function() {
-        paper.setup('myCanvas');
+// connect them with an edge
+graph.newEdge(spruce, fir);
+graph.newEdge(tada,spruce);
+graph.newEdge(fir,tada);
 
-        // Create two drawing tools.
-        // tool1 will draw straight lines,
-        // tool2 will draw clouds.
-
-        // Both share the mouseDown event:
-        var path;
-        function onMouseDown(event) {
-            path = new Path();
-            path.strokeColor = 'black';
-            path.add(event.point);
-        }
-
-        tool1 = new Tool();
-        tool1.onMouseDown = onMouseDown;
-
-        tool1.onMouseDrag = function(event) {
-            path.add(event.point);
-        }
-
-        tool2 = new Tool();
-        tool2.minDistance = 20;
-        tool2.onMouseDown = onMouseDown;
-
-        tool2.onMouseDrag = function(event) {
-            // Use the arcTo command to draw cloudy lines
-            path.arcTo(event.point);
-        }
-    };
+$('#my_canvas').springy({ graph: graph });
